28ba186de9ae1db0cc6861badee95ce181a4ac1a
medkdr <88434000+medkdr@users.noreply.github.com>
2022-06-02 14:17:39 +0100

    q4

--- app/src/main/java/com/example/cc2_kaddour/MyDatabase.java
+++ app/src/main/java/com/example/cc2_kaddour/MyDatabase.java
@@ -7,5 +7,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 public class MyDatabase extends SQLiteOpenHelper{
+    public static String DB_NAME="enterprises.db";
+    public static String TABLE_NAME="Entreprise";
+    public static String COL1="ID";
+    public static String COL2="Raison_Sociale";
+    public static String COL3="Adresse";
+    public static String COL4="Capitale";
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "create table " + TABLE_NAME + "("+COL1+" integer primary key autoincrement,"+COL2+" TEXT,"+COL3+" TEXT,"+COL4+" double)";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
 }
