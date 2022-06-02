c31dd91f2a7a1530e6d4d9bd81ee087b77572e65
medkdr <88434000+medkdr@users.noreply.github.com>
2022-06-02 15:19:08 +0100

    q10

--- app/src/main/java/com/example/cc2_kaddour/MainActivity.java
+++ app/src/main/java/com/example/cc2_kaddour/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cc2_kaddour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Text e1, e2, e3;
+        Button b1 ,b2;
+        MyDatabase db;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            db = new MyDatabase(this);
+            e1 = findViewById(R.id.rais);
+            e2 = findViewById(R.id.adress);
+            e3 = findViewById(R.id.Capitale);
+            b1 = findViewById(R.id.btnsauvgarder);
+            b1 = findViewById(R.id.btnannuler);
+
+
+            ArrayList<Entreprise> ae = MyDatabase.getAllEntreprise(db.getReadableDatabase());
+
+
+            ArrayList<String> listitems = new ArrayList<>();
+            for(Entreprise e : ae)
+                listitems.add(e.getRaison_Sociale());
+
+
+        }
+
+        }
     }
 }
