d4e70fbeb21ff26b9e84b28498fb86104dd92b0f
medkdr <88434000+medkdr@users.noreply.github.com>
2022-06-02 15:52:36 +0100

    q10 .

--- app/src/main/java/com/example/cc2_kaddour/MainActivity.java
+++ app/src/main/java/com/example/cc2_kaddour/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cc2_kaddour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,36 +14,29 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Object View;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Text e1, e2, e3;
-        Button b1 ,b2;
+        Button b1, b2,b3;
         MyDatabase db;
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate (Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            public void acces(View Thread view;
+            view) {
+                Intent i = null;
+                switch ((int) view.getId()){
+                    case R.id.b1: i=new Intent(MainActivity.this, MainActivity.class); break;
+                    case R.id.b2: i=new Intent(MainActivity.this, editeenreprise.class); break;
+                    case R.id.b3: i=new Intent(MainActivity.this, addentreprise.class); break;
+                }
+                startActivity(i);
 
-            db = new MyDatabase(this);
-            e1 = findViewById(R.id.rais);
-            e2 = findViewById(R.id.adress);
-            e3 = findViewById(R.id.Capitale);
-            b1 = findViewById(R.id.btnsauvgarder);
-            b1 = findViewById(R.id.btnannuler);
-
-
-            ArrayList<Entreprise> ae = MyDatabase.getAllEntreprise(db.getReadableDatabase());
-
-
-            ArrayList<String> listitems = new ArrayList<>();
-            for(Entreprise e : ae)
-                listitems.add(e.getRaison_Sociale());
-
-
-        }
-
+            }
         }
     }
 }
