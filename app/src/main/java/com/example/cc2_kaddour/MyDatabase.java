ec0a56730e2229da7d8b1196879491804ca10891
medkdr <88434000+medkdr@users.noreply.github.com>
2022-06-02 14:16:16 +0100

    q3

---
+++ app/src/main/java/com/example/cc2_kaddour/MyDatabase.java
@@ -0,0 +1,11 @@
+package com.example.cc2_kaddour;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+public class MyDatabase extends SQLiteOpenHelper{
+
+}
